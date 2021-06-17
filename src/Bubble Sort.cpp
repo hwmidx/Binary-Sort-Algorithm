605c69beba91e999be932fa71ce4cbeb0c74d880
Srwsh <85651275+Amoo-Srwsh@users.noreply.github.com>
2021-06-17 16:48:44 -0700

    Add a bubble sort file

---
+++ src/Bubble Sort.cpp
@@ -0,0 +1,30 @@
+//bubble sort
+#include <iostream>
+using namespac std;
+
+
+void sort(int [], int);
+main()
+{
+	const int size = 6; 
+	int a[size] = {9,5,3,1,7,4};
+
+	sort(a,size);
+	for(int i=0; i<size; i++)
+		  cout<<a[i]<<'\t';
+}
+
+void sort(int a[], int size)
+{
+  int hold;
+
+	for(int p=0; p<size-1; p++)
+		 for(int j=0; j<size; j++)
+			   if(a[j] > a[j+1])
+			       {
+			      	hold=a[j];
+				      a[j]=a[j+1];
+				      a[j+1]=hold;
+			       }
+}
+	 
