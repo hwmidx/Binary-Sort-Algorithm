7ab1d59ba60734e28dfc5fd86d75d9bbb6c9a576
hwmidx <hwmidx@gmail.com>
2021-07-29 00:22:33 +0430

    update to bubble.cpp

--- src/Bubble Sort.cpp
+++ src/Bubble Sort.cpp
@@ -1,31 +1,28 @@
-//bubble sort
-//Git Hub Amoo-Srwsh
+//Bubble-Sort
+//Github Amoo-Srwsh And hwmidx
+
 #include <iostream>
 using namespace std;
 
-
-void sort(int [], int);
-main()
-{
-	const int size = 6; 
-	int a[size] = {9,5,3,1,7,4};    //Sort (9      5      3      1      7      4)             Out Put (1      3       4       5       6       7)
-
-	sort(a,size);
-	for(int i=0; i<size; i++)
-		  cout<<a[i]<<'\t';
+void bubbleSort(int[], int);
+int main(){
+    const int size = 6;
+    int number[size] = {9, 5, 3, 1, 7, 4};   //Output (1      3       4       5       7       9)
+    bubbleSort(number, size);
+    for (int i = 0; i < size; i++)
+        cout << number[i] << "\t";
+    return 0;
 }
-
-void sort(int a[], int size)
-{
-  int hold;
-
-	for(int p=0; p<size-1; p++)
-		 for(int j=0; j<size; j++)
-			   if(a[j] > a[j+1])
-			       {
-			      	hold=a[j];
-				      a[j]=a[j+1];
-				      a[j+1]=hold;
-			       }
+// Bubble-Sort //
+void bubbleSort(int num[], int len){
+    int temp;
+    for (int p = len - 1; 0 < p; p--)
+        for (int j = 0; j < len; j++){
+            if (num[j] > num[j + 1])
+            {
+                temp = num[j];
+                num[j] = num[j + 1];
+                num[j + 1] = temp;
+            }
+		}
 }
-	 
